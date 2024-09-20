732f6c8361770079f60e0341c6c899690cc2622c
codelife23 <maxntu5@gmail.com>
2024-09-21 00:20:19 +0200

    TypeScript skill was added

--- src/main/Home/Skills/Skills.js
+++ src/main/Home/Skills/Skills.js
@@ -5,8 +5,9 @@ import { SectionNameStyled } from 'styled';
 import Skill from './Skill';
 import html5 from 'assets/html5.png';
 import css3 from 'assets/css3.png';
-import js from 'assets/js.png';
 import reactjs from 'assets/reactjs.png';
+import ts from 'assets/ts.png';
+import js from 'assets/js.png';
 
 function Skills() {
   const { t } = useTranslation('my_skills');
@@ -21,21 +22,25 @@ function Skills() {
           {t('my_expertise')}
         </h2>
 
-        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-x-24 gap-y-48 mt-76">
-          <div className="text-center">
-            <Skill logoSrc={html5} to={90} title="HTML5" />
+        <div className="grid sm:grid-cols-6 lg:grid-cols-5 gap-x-20 gap-y-48 mt-76">
+          <div className="text-center sm:max-lg:col-span-2">
+            <Skill logoSrc={ts} to={80} title="TypeScript" />
           </div>
 
-          <div className="text-center">
-            <Skill logoSrc={css3} to={90} title="CSS3" />
-          </div>
-
-          <div className="text-center">
+          <div className="text-center sm:max-lg:col-span-2">
             <Skill logoSrc={js} to={80} title="JavaScript" />
           </div>
 
-          <div className="text-center">
-            <Skill logoSrc={reactjs} to={80} title="ReactJS" />
+          <div className="text-center sm:max-lg:col-span-2">
+            <Skill logoSrc={reactjs} to={90} title="ReactJS" />
+          </div>
+
+          <div className="text-center sm:max-lg:col-span-2 sm:max-lg:col-start-2">
+            <Skill logoSrc={html5} to={90} title="HTML5" />
+          </div>
+
+          <div className="text-center sm:max-lg:col-span-2">
+            <Skill logoSrc={css3} to={90} title="CSS3" />
           </div>
         </div>
       </div>
